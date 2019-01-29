736e0384ca6e1d41c87c812e65dd2777e4b37799
Zachary Jeffreys <zachary.jeffreys@wsu.edu>
2019-01-29 01:22:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-
+function getFirstSelector(selector){
+  var element = document.querySelector(selector);
+  return(element);
+}
+function nestedTarget(){
+  var element = document.querySelector(`#nested .target`);
+  return element;
+} 
+function increaseRankBy(n){
+  var ranks = document.querySelectorAll(`ul.ranked-list li`);
+  for(var i = 0; ranks.length>i; i++){
+    ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n;
+  }
+}
+function deepestChild(){
+  var grand = document.querySelector(`div#grand-node`)
+}
